67ffc676d885cf17fbb6008bc26659e180bd37a0
anouar <anouar.ab95@gmail.com>
2024-10-01 17:23:53 +0100

    fin page Equipe

---
+++ src/main/java/Projet_Calbo/model/Members.java
@@ -0,0 +1,41 @@
+package Projet_Calbo.model;
+
+public class Members {
+	  private String nom;
+	    private String prenom;
+	    private String email;
+	    private Role role;
+
+	    public String getNom() {
+	        return nom;
+	    }
+
+	    public void setNom(String nom) {
+	        this.nom = nom;
+	    }
+
+	    public String getPrenom() {
+	        return prenom;
+	    }
+
+	    public void setPrenom(String prenom) {
+	        this.prenom = prenom;
+	    }
+
+	    public String getEmail() {
+	        return email;
+	    }
+
+	    public void setEmail(String email) {
+	        this.email = email;
+	    }
+
+	    public Role getRole() {
+	        return role;
+	    }
+
+	    public void setRole(Role role) {
+	        this.role = role;
+	    }
+
+}
