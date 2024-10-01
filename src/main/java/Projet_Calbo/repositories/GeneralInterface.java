c2b005f3b5da08e09c34f17fd42f2e0a6ce82d8f
anouar <anouar.ab95@gmail.com>
2024-10-01 12:14:46 +0100

    GC-34

--- src/main/java/Projet_Calbo/repositories/GeneralInterface.java
+++ src/main/java/Projet_Calbo/repositories/GeneralInterface.java
@@ -1,7 +1,10 @@
 package Projet_Calbo.repositories;
 
+import java.util.List;
+
 public interface GeneralInterface<T> {
     void save(T entity);
     void update(T entity);
     void delete(T entity);
+    List <T> getAll(T entity);
 }
