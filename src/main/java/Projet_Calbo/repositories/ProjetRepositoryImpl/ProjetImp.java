b17e77a9c095fe426f73c4234e428e8b30729859
asmaabarj <asmaabarj5@gmail.com>
2024-10-01 20:01:29 +0100

    GC-46 <modifier le nom des dossieres>

--- src/main/java/Projet_Calbo/repositories/ProjetRepositoryImpl/ProjetImp.java
+++ src/main/java/Projet_Calbo/repositories/ProjetRepositoryImpl/ProjetImp.java
@@ -1,5 +1,26 @@
 package Projet_Calbo.repositories.ProjetRepositoryImpl;
 
-public class ProjetImp {
+import java.util.ArrayList;
+import java.util.List;
 
+import Projet_Calbo.repositories.GeneralInterface;
+
+public class ProjetImpl implements GeneralInterface<Projet> {
+
+    @Override
+    public void save(Projet entity) {
+    }
+
+    @Override
+    public void update(Projet entity) {
+    }
+
+    @Override
+    public void delete(Projet entity) {
+    }
+
+    @Override
+    public List<Projet> getAll() {
+        return new ArrayList<>(); 
+    }
 }
