95100db1d28c0bca67d2c31d4d2c97a016ffdfa8
asmaabarj <asmaabarj5@gmail.com>
2024-10-02 09:37:10 +0100

    connection base données

--- src/main/java/Projet_Calbo/model/Projet.java
+++ src/main/java/Projet_Calbo/model/Projet.java
@@ -11,9 +11,7 @@ public class Projet {
 	private LocalDate dateFin ;
 	private Statut statut;
 	
-	public enum Statut {
-		ENPREPARATION, ENCOURS, ENPAUSE ,TERMINE ,ANNULE
-    }
+	
 	
 	
 	public Projet(int id , String nom ,String Description ,LocalDate dateDebut ,LocalDate dateFin, String description, Statut statut) {
