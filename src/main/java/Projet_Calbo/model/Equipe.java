b4d0e3cfc7e8733febf518a44806f91f9ff88d73
anouar <anouar.ab95@gmail.com>
2024-10-02 09:40:53 +0100

    Genner interface

--- src/main/java/Projet_Calbo/model/Equipe.java
+++ src/main/java/Projet_Calbo/model/Equipe.java
@@ -1,15 +1,28 @@
 package Projet_Calbo.model;
 
+import java.util.List;
+
 public class Equipe {
     private Integer id;
     private String nom;
+    private List<Members> members; // List of Members in the Equipe
 
+    // No-argument constructor
+    public Equipe() {
+    }
 
+    // Parameterized constructor including Members list
+    public Equipe( String nom, List<Members> members) {
+    
+        this.nom = nom;
+        this.members = members;
+    }
+
+    // Getters and Setters
     public Integer getId() {
         return id;
     }
 
-    
     public void setId(Integer id) {
         this.id = id;
     }
@@ -18,8 +31,15 @@ public class Equipe {
         return nom;
     }
 
-   
     public void setNom(String nom) {
         this.nom = nom;
+    }
+
+    public List<Members> getMembers() {
+        return members;
+    }
+
+    public void setMembers(List<Members> members) {
+        this.members = members;
     }
 }
--- src/main/java/Projet_Calbo/repositories/GeneralInterface.java
+++ src/main/java/Projet_Calbo/repositories/GeneralInterface.java
@@ -2,6 +2,10 @@ package Projet_Calbo.repositories;
 
 import java.util.List;
 
-public interface GeneralInterface {
-
+public interface GeneralInterface<T> {
+	boolean save(T entity);
+	void update(T entity);
+	void delete(T entity);
+	List<T> getAll();
+	T findById(Integer id);
 }
